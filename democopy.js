686a6244399b96d5aa6908a5eab1132278ffd1e0
20880115 <you@example.com>
2021-07-23 21:56:39 +0700

    2

---
+++ democopy.js
@@ -0,0 +1,16 @@
+/*
+let username = "per"
+let message = "you have three new nofi"
+let messagetoUser = username + " " + message
+console.log(message)
+
+console.log(4+5)  //9
+console.log("2"+"4")  //24
+console.log("5"+1)  //51
+console.log(100+"100")  //100100
+*/
+
+let name1 = "yen"
+let greeting = "welcome"
+let welcomeEl = name1 + greeting
+welcomeEl = document.getElementById.innerText("welcome-El")
