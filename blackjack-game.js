d6048ff9ee3b7c2af7492a57676030cbf37684de
20880115 <you@example.com>
2021-07-30 21:36:40 +0700

    6

--- blackjack-game.js
+++ blackjack-game.js
@@ -1,21 +1,48 @@
 /* blackjack game */
 
-let firstCard = 3;
-let secondCard = 11;
-let cards = [firstCard, secondCard];
-let sum = firstCard+secondCard;
-let aLive = true;
+let cards = [];
+let sum = 0;
+let aLive = false;
 let message = "";
 let messageEl= document.getElementById("message-el");
 //let sumEl = document.getElementById("sum-el");
 let sumEl = document.querySelector("#sum-el"); // query css selector. class is .  id is #
 let cardsEl = document.getElementById("cards-el");
 
+
+	//gemerate random cards
+	function getrandomcard(){
+		let random= Math.floor(Math.random()*13)+1; // 1 -> 13 
+		
+		if( random ==1 ){
+			return 11;
+		}
+		if( random > 10){
+			return 10;
+		} else{
+			return random;
+		}
+		
+		
+	}
+
+
 	function startgame(){
+		 isAlive = true;
+		 let firstCard = getrandomcard();
+		let secondCard = getrandomcard();
+		sum = firstCard + secondCard;
+		cards = [firstCard, secondCard];
 		rendergame();
 	}
 
 	function rendergame(){
+		cardsEl.textContent = "Cards: "
+
+		
+		for ( let i = 0; i < cards.length; i++){
+			cardsEl.textContent +=  cards[i]+ " ";	
+		}
 
 		if( sum < 21){
 			message = "do you want to draw a new card?";
@@ -30,23 +57,17 @@ let cardsEl = document.getElementById("cards-el");
 		}
 		messageEl.textContent = message;
 		sumEl.textContent = "Sum: "+ sum;
-		cardsEl.textContent = "Card: "+ cards[0]+" "+ cards[1]; 
+		//cardsEl.textContent = "Card: "+ cards[0]+" "+ cards[1]; 
 
 	}
 
 	function newcard(){
-
-		let card = 7;
+		let card = getrandomcard();
 		sum += card;
 		cards.push(card);
 		console.log(cards);
 		rendergame();
-
-		 
-	}
+	} 
 	
 
-	let arrays = [7,6,9];
-	for ( let i =0; i < arrays.length; i++){
-		console.log(arrays[i]);
-	}
+	
