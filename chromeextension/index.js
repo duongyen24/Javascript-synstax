25d3ca4c192e29a8572d9455bd338544e4449097
duongyen24 <duongyen9791@gmail.com>
2021-08-06 08:11:22 +0700

    done

--- chromeextension/index.js
+++ chromeextension/index.js
@@ -19,35 +19,26 @@ console.log(myLeads);
 
 //console.log(leadFromlocalstoreage);
 
-let inputbtn = document.getElementById("input-btn");
-let inputEl = document.getElementById("input-el");
+const inputbtn = document.getElementById("input-btn");
+const inputEl = document.getElementById("input-el");
 const ulEl = document.getElementById("unorder-list");
+const deleteBtn = document.getElementById("delete-btn");
+const tabBtn = document.getElementById("tab-btn");
 
 let myLeads = [];
 const leadFromlocalstoreage = JSON.parse(localStorage.getItem("myLeads"));
 
 //check if true or not
-if( leadFromlocalstoreage){
-	myLeads = leadFromlocalstoreage;
-	renderLoad();
+if (leadFromlocalstoreage) {
+  myLeads = leadFromlocalstoreage;
+  render(myLeads);
 }
 
-inputbtn.addEventListener("click", function () {
-  myLeads.push(inputEl.value);
-  //console.log(myLeads);
-  clear();
-  
-  //local storage
-  localStorage.setItem("myLeads", JSON.stringify(myLeads)); //(key , value)
-  console.log(localStorage.getItem("myLeads"));
-  //localStorage.clear();
-  renderLoad();
-});
-
-function renderLoad() {
+//we try to make this function reuse and dynamic
+function render(leads) {
   let listItems = " ";
 
-  for (let i = 0; i < myLeads.length; i++) {
+  for (let i = 0; i < leads.length; i++) {
     /* another way of innerHTML
 	create element
 	const li = document.createElement("li");
@@ -61,16 +52,42 @@ function renderLoad() {
     //modify the open link in new tab with template strings
     listItems += `
 		<li>
-			<a target='_blank' href=' ${myLeads[i]}'>
-			${myLeads[i]} 
+			<a target='_blank' href=' ${leads[i]}'>
+			${leads[i]} 
 			</a>
 		</li>`;
   }
   ulEl.innerHTML = listItems;
 }
 
-function clear() {
-  if (inputEl.value !== "") {
-    inputEl.value = "";
-  }
-}
+inputbtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+  //console.log(myLeads);
+  inputEl.value = "";
+
+  //local storage
+  localStorage.setItem("myLeads", JSON.stringify(myLeads)); //(key , value)
+  console.log(localStorage.getItem("myLeads"));
+  //localStorage.clear();
+  render(myLeads);
+});
+
+//modify delete btn
+deleteBtn.addEventListener("click", function () {
+  localStorage.clear();
+  myLeads = [];
+  render(myLeads);
+});
+
+//log out nav link
+//const tabs = [{ url: "https://www.youtube.com" }];
+tabBtn.addEventListener("click", function () {
+  //console.log(tabs[0].url);
+  //render the link in tabs to UI, using chrome API to get the url at current tab
+  //query chrome API
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    myLeads.push(tabs[0].url);
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+  });
+});
