85872dc61800ef2a6d1f15d9129bc4d81c343c39
duongyen24 <duongyen9791@gmail.com>
2021-08-18 22:53:52 +0700

    11

--- chapter8/app.js
+++ chapter8/app.js
@@ -1,9 +1,20 @@
 const a = [
 	{name:'a' , price:40},
 	{name:'b', price:50},
-	{name: 'c', price:90},
+	{name: 'a', price:90},
 	{name: 'd', price:30}
 ];
+
+const filtered = a.filter(product1 =>{
+	return product1.price>40;
+});
+console.log(filtered);
+
+const mapped = filtered.map(map =>{
+	return `the price is ${map.price}`;
+});
+console.log(mapped);
+
 
 const temp = a.map( (data) =>{
 	if(data.price>= 40){
@@ -15,3 +26,23 @@ const temp = a.map( (data) =>{
 })
 
 console.log(temp);
+console.log(a);
+
+const reduceMethod = a.reduce((acc,curr)=>{
+	if(curr.name === 'a'){
+		acc += curr.price;
+	}
+	return acc;
+},0);
+
+console.log(reduceMethod);
+console.log(a);
+
+
+const b = [30,100,60,90];
+const findMethod = b.find((price) =>{
+	return price > 50;
+});
+
+console.log(findMethod);
+
